0114950e3151c5e6c25ce49d227195097221e84f
LAPTOP-I2IVEE82\ymy06 <ymy0613@gmail.com>
2023-11-28 21:09:25 +0900

    p태그 지움

--- test2/test333/src/App.tsx
+++ test2/test333/src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
@@ -14,9 +12,6 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
